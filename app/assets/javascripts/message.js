2d43c1a4a256d7488e309afca33c3087e670123d
yukito1005 <tokuoka.yukito@gmail.com>
2020-02-11 20:13:19 +0900

    test

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,65 @@
-$(function(){ 
+$(function(){
+  last_message_id = $('.message:last').data("message-id");
+  console.log(last_message_id);
   function buildHTML(message){
    if ( message.image ) {
-     var html =
-      `<div class="main-content" data-message-id=${message.id}>
-         <div class="main-content__user">
-           <div class="user_name">
-             ${message.user_name}
-           </div>
-           <div class="date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="main-content__comment">
-           <p class="comment">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="main-content" data-message-id=${message.id}>
-         <div class="main-content__user">
-           <div class="user_name">
-             ${message.user_name}
-           </div>
-           <div class="udate">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="main-content__comment">
-           <p class="comment">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
+     var buildHTML = function(message) {
+    if (message.content && message.image) {
+      //data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.content) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    };
+    return html;
+  };
  }
-$('#new_message').on('submit', function(e){
+
+$('#new_message').on('submit', function(e){x
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
@@ -62,5 +82,33 @@ $('#new_message').on('submit', function(e){
     alert("メッセージ送信に失敗しました");
   });
  })
-});
+ var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    //ルーティングで設定した通りのURLを指定
+    url: "api/messages",
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    //追加するHTMLの入れ物を作る
+    var insertHTML = '';
+    //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+    $.each(messages, function(i, message) {
+      insertHTML += buildHTML(message)
+    });
+    //メッセージが入ったHTMLに、入れ物ごと追加
+    $('.messages').append(insertHTML);
+  })
+  .fail(function() {
+    console.log('error');
+  });
+};
+
+
+})
 
