7523b2fb878b38f407a31ecfddcd016fe5802871
yukito1005 <tokuoka.yukito@gmail.com>
2020-02-11 20:56:50 +0900

    ぷるりくえすと
    あえふぇ

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,7 @@
 $(function(){
   last_message_id = $('.message:last').data("message-id");
   console.log(last_message_id);
+
   function buildHTML(message){
    if ( message.image ) {
      var buildHTML = function(message) {
@@ -59,7 +60,7 @@ $(function(){
   };
  }
 
-$('#new_message').on('submit', function(e){x
+$('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
@@ -71,6 +72,7 @@ $('#new_message').on('submit', function(e){x
    processData: false,
    contentType: false
  })
+
   .done(function(data){
     var html = buildHTML(data);
     $('.main-content').append(html);      
@@ -78,10 +80,12 @@ $('#new_message').on('submit', function(e){x
     $('.main-content').animate({ scrollTop: $('.main-content')[0].scrollHeight});
     $('.send-top').prop('disabled',false);
   })
+
   .fail(function() {
     alert("メッセージ送信に失敗しました");
   });
  })
+
  var reloadMessages = function() {
   //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
   last_message_id = $('.message:last').data("message-id");
@@ -95,6 +99,7 @@ $('#new_message').on('submit', function(e){x
     data: {id: last_message_id}
   })
   .done(function(messages) {
+    if (messages.length !== 0) {
     //追加するHTMLの入れ物を作る
     var insertHTML = '';
     //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
@@ -103,10 +108,13 @@ $('#new_message').on('submit', function(e){x
     });
     //メッセージが入ったHTMLに、入れ物ごと追加
     $('.messages').append(insertHTML);
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
   })
   .fail(function() {
     console.log('error');
   });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
 };
 
 
